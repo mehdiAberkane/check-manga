0f72c7b815838aae9000ccd794a1485dd4f5eda8
Mehdi <mehdi@mehdi-aberkane.fr>
2022-05-01 01:47:08 +0200

    some stuff

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import feedparser
+import requests
+import os
+import logging
+
+from logging.handlers import TimedRotatingFileHandler
+
+logname = "check_manga.log"
+handler = TimedRotatingFileHandler(logname, when="midnight", interval=1)
+handler.suffix = "%Y%m%d"
+logger = logging.getLogger('simple')
+logger.addHandler(handler)
+
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig(level="INFO")
+
+discordUrl = os.environ.get("url")
+
+news_feed = feedparser.parse('https://www.japscan.ws/rss/')
+
+list_manga = ['boku-girl', 'my-hero-academia', 'the-constellation-only-like-me', 'kill-the-dragon']
+
+news = []
+
+check_manga = open("check_manga.log", "r")
+with open('check_manga.log') as f:
+    lines = f.read().splitlines()
+
+for entry in news_feed.entries:
+    if entry.link not in lines:
+        if any(ext in entry.link for ext in list_manga):
+            logger.info(entry.link)
+            news.append(entry.link)
+
+
+if len(news) > 0:
+    response = requests.post(discordUrl, data={"content": "@everyone "+', '.join(news)})
